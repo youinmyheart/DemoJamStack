9ee4e4b074f1ea8d9eddbcf7de23526dbf373c1b
Phong <tuanphong1081985@gmail.com>
2020-06-20 00:09:50 +0700

    Add Swift project

---
+++ DemoJamStack/ViewController.swift
@@ -0,0 +1,18 @@
+// 
+// ViewController.swift
+// 
+// Created on 6/19/20.
+// 
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidLoad")
+    }
+
+
+}
+
